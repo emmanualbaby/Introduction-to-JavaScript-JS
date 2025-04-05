3506a65da51363a40463c630d3332c01b25e2536
Emmanual Baby <menothemmanualbaby@gmail.com>
2025-04-05 21:41:40 +0530

    Add files via upload

---
+++ 02_Where To.js
@@ -0,0 +1,13 @@
+<!-- JS Where To - Example -->
+<!DOCTYPE html>
+<html>
+<head>
+  <title>JavaScript Example</title>
+</head>
+<body>
+  <h1>Hello, World!</h1>
+  <script>
+    document.write("This is JavaScript inside the body!"); // Write text in the body
+  </script>
+</body>
+</html>
