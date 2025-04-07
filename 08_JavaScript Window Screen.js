b1ba6de209e9a99949bcade59f5ef52086ba3ea1
Emmanual Baby <menothemmanualbaby@gmail.com>
2025-04-07 16:12:38 +0530

    Add files via upload

---
+++ 08_JavaScript Window Screen.js
@@ -0,0 +1,23 @@
+// Screen Properties
+
+// Get the screen width and height
+let screenWidth = window.screen.width;
+let screenHeight = window.screen.height;
+console.log("Screen Width: " + screenWidth + ", Screen Height: " + screenHeight);
+// Output: Screen Width: [screen width], Screen Height: [screen height]
+
+// Get the available screen width and height
+let availWidth = window.screen.availWidth;
+let availHeight = window.screen.availHeight;
+console.log("Available Screen Width: " + availWidth + ", Available Screen Height: " + availHeight);
+// Output: Available Screen Width: [available screen width], Available Screen Height: [available screen height]
+
+// Get the color depth of the screen
+let colorDepth = window.screen.colorDepth;
+console.log("Color Depth: " + colorDepth);
+// Output: Color Depth: [color depth]
+
+// Get the pixel depth of the screen
+let pixelDepth = window.screen.pixelDepth;
+console.log("Pixel Depth: " + pixelDepth);
+// Output: Pixel Depth: [pixel depth]
