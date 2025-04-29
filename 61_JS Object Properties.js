39885718c0aab8faf8a0d942644398e978c8e8e7
Emmanual Baby <menothemmanualbaby@gmail.com>
2025-04-29 22:21:18 +0530

    Add files via upload

---
+++ 61_JS Object Properties.js
@@ -0,0 +1,12 @@
+// JS Object Properties - Example
+let person = {
+  firstName: "John",
+  lastName: "Doe",
+  age: 30,
+  eyeColor: "blue"
+};
+
+console.log("First Name: " + person.firstName);
+console.log("Last Name: " + person.lastName);
+console.log("Age: " + person.age);
+console.log("Eye Color: " + person.eyeColor);
