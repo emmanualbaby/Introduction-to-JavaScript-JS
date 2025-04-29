2b7e8e639e9babca939cea25c34a6506061b101c
Emmanual Baby <menothemmanualbaby@gmail.com>
2025-04-29 22:21:36 +0530

    Add files via upload

---
+++ 62_JS Data Types.js
@@ -0,0 +1,12 @@
+// JS Data Types - Example
+let number = 100; // Number
+let name = "Alice"; // String
+let isStudent = true; // Boolean
+let car = {type: "Fiat", model: "500", color: "white"}; // Object
+let undefinedVariable; // Undefined
+
+console.log("Number: " + number);
+console.log("String: " + name);
+console.log("Boolean: " + isStudent);
+console.log("Object: ", car);
+console.log("Undefined: " + undefinedVariable);
