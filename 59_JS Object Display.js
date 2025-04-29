e00e51ed33a49f2a4102bce7157ee6265af6d15e
Emmanual Baby <menothemmanualbaby@gmail.com>
2025-04-29 22:20:16 +0530

    Add files via upload

---
+++ 59_JS Object Display.js
@@ -0,0 +1,10 @@
+// JS Object Display - Example
+let car = {
+  make: "Toyota",
+  model: "Corolla",
+  year: 2020
+};
+
+console.log("Using console.log:", car); // Using console.log
+document.write("Using document.write: " + JSON.stringify(car)); // Using document.write
+alert("Using alert: " + JSON.stringify(car)); // Using alert
