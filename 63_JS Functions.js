5ea09d850c5a2764db2091c0d2110ac0a2b55a30
Emmanual Baby <menothemmanualbaby@gmail.com>
2025-05-03 08:05:35 +0530

    Add files via upload

---
+++ 63_JS Functions.js
@@ -0,0 +1,7 @@
+// JS Functions - Example
+function myFunction(p1, p2) {
+  return p1 * p2; // Function returns the product of p1 and p2
+}
+
+let result = myFunction(4, 3); // Call the function
+console.log("Result: " + result); // Print the result
