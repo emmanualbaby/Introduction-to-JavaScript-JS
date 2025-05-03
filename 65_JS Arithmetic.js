19f4d44a6d9f078eada98d94c49c93cf99942334
Emmanual Baby <menothemmanualbaby@gmail.com>
2025-05-03 08:06:05 +0530

    Add files via upload

---
+++ 65_JS Arithmetic.js
@@ -0,0 +1,14 @@
+// JS Arithmetic - Example
+let a = 10;
+let b = 20;
+let sum = a + b;
+let diff = a - b;
+let prod = a * b;
+let quot = a / b;
+let mod = a % b;
+
+console.log("Sum: " + sum);
+console.log("Difference: " + diff);
+console.log("Product: " + prod);
+console.log("Quotient: " + quot);
+console.log("Modulus: " + mod);
